4dd056d56450e0fd630b6dbb7425f8e9772beaa0
George-whyte <gwhyte@qa.com>
2021-06-25 14:44:18 +0100

    runner example

---
+++ Runner.java
@@ -0,0 +1,16 @@
+package com.qa.helloworld;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+
+		StringManipulation.method();
+		StringManipulation.string2();
+		Arrays.main(args);
+		FizzBuzz.fizzBuzz (5);
+		flowchartproj.flowchart(4000);
+		
+		TaxCalc.main(args);
+	}
+}
